ff2c7ee352f9000436bbae3dfca05b083f9ee529
teddyfg <teddyfg@amazon.co.uk>
2021-03-20 09:53:23 +0000

    created folder structure

--- Topics/dictionary_hashtables/day1.py
+++ Topics/dictionary_hashtables/day1.py
@@ -9,21 +9,23 @@ l3.sort()
 #print l3
 
 #find the indexes of numbers within a list such that the values of the 3 numbers adds up to target. target = 7 for this
+l1 = [1,2,4,6,8,9]
 
 target = 7
 complementHashMap = {}
 index = 0
 for i in l1:
+    print "i is: " + str(i)
     if i>len(l1):
         print "not today, mate"
         break
     complement = target - i
     if complement in complementHashMap:
-        print index
-        print complementHashMap.get(index)
+        print "successful index is: " + str(index)
+        print "and other number is at index: " + str(complementHashMap.get(complement))
         break
     else:
-        complementHashMap[complement]=index
+        complementHashMap[i]=index
         print "index is: " + str(index)
         print "complement is: " + str(complement)
         #print ""
