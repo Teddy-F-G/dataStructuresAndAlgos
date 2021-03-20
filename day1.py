0f9149f939a404d8aa08800780d1e76e22eee9cb
teddyfg <teddyfg@amazon.co.uk>
2021-03-20 09:03:23 +0000

    day 1 learnings, arrays in JS & Py

---
+++ day1.py
@@ -0,0 +1,9 @@
+l1 = [1,2,4]
+l2 = [1,3,4]
+l3 = l1+l2
+l4 = l1.append(l2)
+
+l3.sort()
+print l4 #.sort()
+
+print l3
---
+++ day1.ts
@@ -0,0 +1,27 @@
+//this is an array:
+/*
+
+const tedsArray = [
+    'value1',
+    'value2'
+];
+const secondArray = new Array("hello", "mate", 5);
+console.log(secondArray);
+
+let variable = 2;
+
+tedsArray[1] = 'value3';
+tedsArray.push(5);
+console.log(tedsArray);
+
+variable = 1;
+console.log(variable);
+*/
+
+//coding task 1
+//this is an array:
+let list1 = [1,2,4];
+let list2 = [2,3,5];
+let list3 = list1.concat(list2);
+list3.sort()
+console.log(list3);
